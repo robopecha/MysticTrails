bd35b0e89d3f08900d08aa30e788a8c7462a0444
robopecha <julikolb@gmail.com>
2023-06-11 22:37:52 +0200

    image added

---
+++ tiled files/solaria demo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="solaria demo" tilewidth="16" tileheight="16" tilecount="420" columns="28">
+ <image source="../../../Desktop/game assets/Solaria Demo Pack Update 03/16x16/Tilesets/Solaria Demo Update 01.png" width="448" height="240"/>
+</tileset>
